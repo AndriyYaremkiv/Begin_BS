6bfad335bb00c6b7e77179808cda53f5e6c3b501
Andriy Yaremkiv <Andriy_Yaremkiv@epam.com>
2020-01-21 09:42:40 +0200

    Added View, HomeController, Global.asax

---
+++ Models/BookDbInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BookStore.Models
+{
+    public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext db)
+        {
+            db.Books.Add(new Book { Name = "Кобзар", Author = "Т.Шевченко", Price = 220 });
+            db.Books.Add(new Book { Name = "Каменяр", Author = "І.Франко", Price = 180 });
+            db.Books.Add(new Book { Name = "Лісова пісня", Author = "Л.Українка", Price = 190 });
+
+            base.Seed(db);
+        }
+    }
+}
